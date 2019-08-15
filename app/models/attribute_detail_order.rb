0932f78f523f099e5e36a1cbc049a940518f1430
Luis <lucho7722@gmail.com>
2019-08-15 03:02:59 -0300

    Creación de ValorAtributoDetallePedido y la relación con AtributoDetallePedido

--- app/models/attribute_detail_order.rb
+++ app/models/attribute_detail_order.rb
@@ -3,4 +3,5 @@ class AttributeDetailOrder < ApplicationRecord
   validates :attribute_type, presence: true
 
   has_one :order_type
+  has_many :value_attribute_detail_orders
 end
---
+++ test/models/value_attribute_detail_order_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ValueAttributeDetailOrderTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
