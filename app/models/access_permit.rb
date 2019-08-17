a6958a8d75696ab3feeb2d66f027e1d1e57635ad
Facundo Chaud <faku_imper@hotmail.com.ar>
2019-08-17 02:27:33 -0300

    user_profile resource generated, create method implemented

--- app/models/access_permit.rb
+++ app/models/access_permit.rb
@@ -1,6 +1,8 @@
 # PermisoAcceso
 class AccessPermit < ApplicationRecord
+  attribute :use_case_name, :string
   validates :use_case_name, presence: true
+  attribute :use_case_url, :string
   validates :use_case_url, presence: true
 
   has_and_belongs_to_many :user_profiles
