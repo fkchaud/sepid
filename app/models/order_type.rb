f3a5e97262dc242a6691535d5282a73485d37b16
Luis <lucho7722@gmail.com>
2019-08-21 21:46:44 -0300

    Agregar atributos de forma dinámica en el tipo de pedido

--- app/models/order_type.rb
+++ app/models/order_type.rb
@@ -6,4 +6,6 @@ class OrderType < ApplicationRecord
   has_many :orders
   has_many :order_detail_attributes
   has_many :order_type_attributes
+  accepts_nested_attributes_for :order_type_attributes, allow_destroy: true
+  accepts_nested_attributes_for :order_detail_attributes, allow_destroy: true
 end
