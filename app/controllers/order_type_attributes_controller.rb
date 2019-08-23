4b1549c69245415bf668befadf612d41a4b25e21
Luis <lucho7722@gmail.com>
2019-08-22 22:55:42 -0300

    Parametrización, falta terminar la edición

--- app/controllers/order_type_attributes_controller.rb
+++ app/controllers/order_type_attributes_controller.rb
@@ -4,6 +4,16 @@ class OrderTypeAttributesController < ApplicationController
     @order_type_attribute = @order_type.order_type_attributes.create(order_type_attribute_params)
     redirect_to order_type_path(@order_type)
   end
+  def edit
+    @order_type = OrderType.find(params[:order_type_id])
+    @order_type_attribute = @order_type.order_type_attributes.find(params[:id])
+  end
+  def update
+    @order_type = OrderType.find(params[:order_type_id])
+    @order_type_attribute = @order_type.order_type_attributes.find(params[:id])
+    @order_type_attribute.update
+    redirect_to order_type_path(@order_type)
+  end
   def destroy
     @order_type = OrderType.find(params[:order_type_id])
     @order_type_attribute = @order_type.order_type_attributes.find(params[:id])
