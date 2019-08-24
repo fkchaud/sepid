2ddc0974f67405682de8f4f7f582fd4e5712dabf
Facundo Chaud <faku_imper@hotmail.com.ar>
2019-08-24 17:43:09 -0300

    mostrar errores en todos los campos de date_select; ABM Proyecto en proceso, actualizado index y _model, hecho _form y new

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,23 +1,18 @@
 # Proyecto
 class Project < ApplicationRecord
+  # attributes
   attribute :project_code, :string
-  validates :project_code, presence: true
   attribute :project_name, :string
-  validates :project_name, presence: true
   attribute :project_description, :string
   attribute :start_date, :date
-  validates :start_date, presence: true
-  validates_date :start_date, before: -> { ending_date }
   attribute :ending_date, :date
-  validates :ending_date, presence: true
-  validates_date :ending_date, after: -> { start_date}
   attribute :technological_scientific_unit, :string
   attribute :project_program, :string
   attribute :activity_type, :string
 
+  # references
   belongs_to :project_status
   belongs_to :project_type
-  # TODO: testear si andan estos dos
   belongs_to :director,
              class_name: 'User',
              inverse_of: :projects_as_director
@@ -28,4 +23,27 @@ class Project < ApplicationRecord
   accepts_nested_attributes_for :codirector
   has_many :project_funds_details
   has_many :orders
+
+  # validations
+  validates :project_code, presence: true
+  validates :project_name, presence: true
+  validates :start_date, presence: true
+  validates_date :start_date
+  validates :ending_date, presence: true
+  validates_date :ending_date
+  validate :ending_date_after_start_date, on: :create
+
+  def ending_date_after_start_date
+    if start_date.present? &&
+       ending_date.present? &&
+       start_date >= ending_date
+      errors.add(:start_date, "can't be after end date")
+      errors.add(:ending_date, "can't be before start date")
+    end
+  end
+
+  def disabled?
+    project_status.name == 'Cancelado' || project_status.name == 'Finalizado'
+  end
+
 end
