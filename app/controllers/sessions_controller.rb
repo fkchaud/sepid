b0a172e2ab2923c19337c816e2427aa1ce66c2ca
Facundo Chaud <faku_imper@hotmail.com.ar>
2019-08-24 23:53:36 -0300

    index alterna entre Iniciar y Cerrar sesión

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,24 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(user_name: params[:session][:name].downcase)
     if user && user.authenticate(params[:session][:password])
-      if !user.is_disabled.nil?
-        flash[:error] = "Usuario no habilitado, comuníquese con el administrador del sistema"
+      unless user.is_disabled.nil?
+        flash[:error] = 'Usuario no habilitado, comuníquese con el administrador del sistema'
         redirect_to login_path
         return
       end
       log_in user
       redirect_to welcome_index_path
     else
-      flash[:error] = "Nombre de usuario y/o contraseña incorrectos"
+      flash[:error] = 'Nombre de usuario y/o contraseña incorrectos'
       render 'new'
     end
   end
 
   def destroy
     log_out
-    redirect_to login_path
+    redirect_to welcome_index_path
   end
 end
