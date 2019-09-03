f93f0473e70cd51a9dd7aef6e0eec844c090da92
Carceller Javier <javitocarceller@gmail.com.ar>
2019-09-03 17:56:18 -0300

    Pruebas Unitarias (5 de las cuales 2 son fallidas)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "verificar mail de usuario" do
     assert_equal "pablobaldini1994@gmail.com", users(:one).email
   end
- 
+
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "verificar mail de usuario" do
+    assert_equal "pablobaldini1994@gmail.com", users(:one).email
+  end
+
+  test "verificar longitud telefono" do
+    assert_equal 10, users(:two).telephone.length
+  end
+
+  test "verificar telefono" do
+    assert_equal 2614056798, users(:two).telephone
+  end
+
+  test "verificar nombre mayus" do
+    assert_equal "Luis", users(:three).first_name # en el fixture el nombre esta en minuscula
+  end
+
+  test "verificar nombre cargo universitario" do
+    id_Cargo_Universitario=users(:two).university_position_id #busco la relacion de cargo universitario con usuario
+    assert_equal "profesor",university_positions(id_Cargo_Universitario).name #busco nombre de cargo universitario
+  end
+
 end
