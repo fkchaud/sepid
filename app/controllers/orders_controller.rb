d047ac2570d1b5a613353815558ca1a27cc2c0e6
Luis <lucho7722@gmail.com>
2019-09-03 04:48:39 -0300

    Realizar pedido, avance en la lógica

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,18 @@ class OrdersController < ApplicationController
   def new
     @project = Project.find(params[:project_id])
     @order = Order.new
+    @order_types = OrderType.where(is_disabled: nil)
+  end
+  def create
+    render plain: params[:order][:order_type_id].inspect
+    @order_type = OrderType.find(params[:order][:order_type_id])
+    @project = Project.find(params[:project_id])
+    @order = Order.new
     @order.order_date = Time.now.strftime("%d/%m/%Y")
     @order.project = @project
     @order_status =  OrderStatus.where(order_status_name:'Solicitado', is_disabled: nil).take
     @order_status_history = @order.order_status_histories.new(date_change_status_order: Time.now,
-                                      reason_change_status_order:'Pedido iniciado')
+                                                              reason_change_status_order:'Pedido iniciado')
     @order_status_history.order_status = @order_status
-    @order_types = OrderType.where(is_disabled: nil)
   end
 end
