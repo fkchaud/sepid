19a31dbe14e5e1c7ade1077e3c20a7f3886529d0
pablo baldini <pablobaldini94@hotmail.com>
2019-09-03 18:41:34 -0300

    Merge branch 'master' of C:\Users\13 U104\RubymineProjects\sepid with conflicts.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,7 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "verificar mail de usuario" do
-    assert_equal "pablobaldini1994@gmail.com", users(:one).email
-  end
+
 
   end
 
