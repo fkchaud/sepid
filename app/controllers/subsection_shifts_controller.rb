e92a66c856f0e5c3114e936ea8874c1a05b6f399
Facundo Chaud <faku_imper@hotmail.com.ar>
2019-09-06 03:09:09 -0300

    subsection_shift new, falta index y falta aprobar

--- app/controllers/subsection_shifts_controller.rb
+++ app/controllers/subsection_shifts_controller.rb
@@ -1,42 +1,56 @@
 class SubsectionShiftsController < ApplicationController
+  before_action :set_projects,
+                only: [:new, :create]
 
   def new
-    @project = Project.find params[:project_id]
-    @total_credits = get_total_credits @project
-    @total_expenses = get_total_expenses @project
-    @available_credits = get_available_credits @total_credits, @total_expenses
+    @subsection_shift = SubsectionShift.new
+    Subsection.enabled.each do |subsection|
+      @subsection_shift
+        .project_funds_details
+        .build(
+          year: Time.now.year,
+          subsection: subsection,
+          project: @project
+        )
+    end
+  end
+
+  def create
+    @subsection_shift = SubsectionShift.new subsection_shift_params
+    @subsection_shift.requested_date = Time.now
+    SubsectionShiftStatusHistory.create(
+      date: Time.now,
+      subsection_shift: @subsection_shift,
+      subsection_shift_status: SubsectionShiftStatus.enabled.find_by_name('Solicitado')
+    )
+    if @subsection_shift.save
+      render plain: params.inspect
+    else
+      render 'new'
+    end
   end
 
   private
 
-  def get_total_credits(project)
-    project_funds_details = project.project_funds_details.where year: Time.now.year
-    total_credits_per_subsection = {}
-    total_credits_per_subsection.default = 0
-    project_funds_details.each do |pfd|
-      total_credits_per_subsection[pfd.subsection] += pfd.funds_amount
-    end
-    total_credits_per_subsection
+  def subsection_shift_params
+    params
+      .require(:subsection_shift)
+      .permit(
+        :requested_cause,
+        project_funds_details_attributes: [
+          :funds_amount,
+          :year,
+          :subsection_id,
+          :project_id
+        ]
+      )
   end
 
-  def get_total_expenses(project)
-    orders = project.orders.where('extract(year from order_date) = ?', Time.now.year)
-    valid_orders = orders.reject do |o|
-      ['Cancelado', 'Rechazado'].include? o.order_status.order_status_name
-    end
-    order_details = valid_orders.order_details
-    total_expenses_per_subsection = {}
-    total_expenses_per_subsection.default = 0
-    order_details.each do |od|
-      total_expenses_per_subsection[od.subsection] += od.last_value.amount
-    end
-    total_expenses_per_subsection
-  end
-
-  def get_available_credits(credits, expenses)
-    available_credits_per_subsection = {}
-    credits.each_key { |key| available_credits_per_subsection[key] = credits[key] - expenses[key] }
-    available_credits_per_subsection
+  def set_projects
+    @project = Project.find params[:project_id]
+    @total_credits = @project.total_credits
+    @total_expenses = @project.total_expenses
+    @available_credits = @project.available_credits @total_credits, @total_expenses
   end
 
 end
--- app/models/subsection_shift.rb
+++ app/models/subsection_shift.rb
@@ -1,7 +1,29 @@
 # CambioInciso
 class SubsectionShift < ApplicationRecord
-  validates :requested_date, presence: true
+  # attributes
+  attribute :requested_date, :datetime
+  attribute :requested_cause, :string
 
+  # relationships
   has_many :project_funds_details
+  accepts_nested_attributes_for :project_funds_details, reject_if: :all_blank
   has_many :subsection_shift_status_histories
+
+  def last_status
+    subsection_shift_status_histories.last.subsection_shift_status
+  end
+
+  # validations
+  validates :requested_date, presence: true
+  validates :requested_cause, presence: true
+  validate :all_subsections_sum_zero
+
+  def all_subsections_sum_zero
+    suma = project_funds_details.sum { |pfd| pfd.funds_amount }
+    if suma != 0
+      project_funds_details.each do |pfd|
+        pfd.errors.add :funds_amount, 'must sum 0'
+      end
+    end
+  end
 end
