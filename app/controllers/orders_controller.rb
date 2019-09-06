7d6778bd87eea8a0d910c1874b92ac69e73699a7
Luis <lucho7722@gmail.com>
2019-09-06 00:00:00 -0300

    Realizar pedido paso de parámetros, inicio de la lógica para guardar el pedido

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,18 +6,20 @@ class OrdersController < ApplicationController
   end
   def create
     render plain: params[:order].inspect
-
-  end
-  def continue
-    #render plain: params[:order][:order_type_id].inspect
-    @order_type = OrderType.find(params[:order][:order_type_id])
-    @project = Project.find(params[:project_id])
     @order = Order.new
-    @order.order_date = Time.now.strftime("%d/%m/%Y")
-    @order.project = @project
+    @order_type = OrderType.find(params[:order][:order_type_id])
+    @project = Project.find(params[:order][:project_id])
     @order_status =  OrderStatus.where(order_status_name:'Solicitado', is_disabled: nil).take
+    @order.project = @project
     @order_status_history = @order.order_status_histories.new(date_change_status_order: Time.now,
                                                               reason_change_status_order:'Pedido iniciado')
     @order_status_history.order_status = @order_status
+    @order.order_date = Time.now.strftime("%d/%m/%Y")
+
+    end
+  def continue
+    @order_type = OrderType.find(params[:order][:order_type_id])
+    @project = Project.find(params[:project_id])
+    @order = Order.new
   end
 end
