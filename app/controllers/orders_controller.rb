2d51a289d9991795c5de4b5f388a5915cace2d35
Luis <lucho7722@gmail.com>
2019-09-12 03:05:09 -0300

    Realizar pedido, guardar los detalles y los atributos del detalle, falta asociar el historial del valor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
     @order_types = OrderType.where(is_disabled: nil)
   end
   def create
-    # render plain: params[:order].inspect
+    # render plain: params[:order][:order_details_attributes].inspect
     # Crear el pedido
     @order = Order.new(order_params)
     # Setear la fecha del pedido
@@ -14,17 +14,33 @@ class OrdersController < ApplicationController
     @order.project = Project.find(params[:order][:project_id])
     # Buscar el tipo de pedido seleccionado
     @order_type = OrderType.find(params[:order][:order_type_id])
+    # Asociar historial
+    @order.order_status_histories.new(date_change_status_order: Time.now,
+                                      reason_change_status_order: "Pedido realizado",
+                                      order_status: OrderStatus.where(order_status_name: "Pedido realizado").take)
+    # Guardar el pedido
     # Validar el pedido
     unless @order.valid?
       # Mandar mensaje de error y redireccionar
     end
     (0...(@order_type.order_type_attributes.length)).each do |index|
+      # Crear los valores de los atributos
       @current_order_attribute = @order.order_type_attribute_values.new(value: params[:order][:attribute_names][index])
       unless @current_order_attribute.valid?
         # Mandar mensaje de error y redireccionar
       end
+      # Asociar el valor del atributo con el atributo
       @current_order_attribute.order_type_attribute = @order_type.order_type_attributes[index]
-      @current_order_attribute.save!
+    end
+    # Crear cada detalle con sus atributos y relaicones
+    params[:order][:order_details_attributes].each do |key, value|
+      # Cambiar la siguiente línea de código en el futuro
+      @current_detail = @order.order_details.new(description_detail: "Sanata")
+      (0...@order_type.order_detail_attributes.length).each do |index|
+        @current_detail_attribute = @current_detail.order_detail_attribute_values.new(value: value[:attribute_names][index])
+        @current_detail_attribute.order_detail_attribute = @order_type.order_detail_attributes[index]
+      end
+      @current_detail.subsection = Subsection.find(params[:order][:subsection_id])
     end
   end
   def continue
--- app/models/value_history.rb
+++ app/models/value_history.rb
@@ -3,6 +3,6 @@ class ValueHistory < ApplicationRecord
   validates :amount, presence: true
   validates :date, presence: true
 
-  has_one :value_status
+  belongs_to :value_status
   belongs_to :order_detail
 end
