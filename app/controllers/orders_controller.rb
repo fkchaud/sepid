25c73a1808ce7bec6075adc6c8980de6ea7186b8
Luis <lucho7722@gmail.com>
2019-09-12 23:35:30 -0300

    Realizar pedido, falta asociar el historial del valor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,10 +18,22 @@ class OrdersController < ApplicationController
     @order.order_status_histories.new(date_change_status_order: Time.now,
                                       reason_change_status_order: "Pedido realizado",
                                       order_status: OrderStatus.where(order_status_name: "Pedido realizado").take)
-    # Guardar el pedido
     # Validar el pedido
     unless @order.valid?
       # Mandar mensaje de error y redireccionar
+    end
+    # Buscar todas las instancias de incisos
+    @subsections = Subsection.enabled.all
+    # Crear un hash en el que guardar los montos totales de cada inciso
+    @amounts = {}
+    @subsections.each do |x|
+      @amounts[x.name] = 0.0
+    end
+    # Buscar todos los detalles de fondos del proyecto actual
+    @project_founds_details = ProjectFundsDetail.where(year: Time.now.year, project: @project)
+    # Calcular todos los montos disponibles
+    @project_founds_details.each do |founds_detail|
+      @amounts[founds_detail.subsection.name] = founds_detail.funds_amount
     end
     (0...(@order_type.order_type_attributes.length)).each do |index|
       # Crear los valores de los atributos
@@ -40,7 +52,40 @@ class OrdersController < ApplicationController
         @current_detail_attribute = @current_detail.order_detail_attribute_values.new(value: value[:attribute_names][index])
         @current_detail_attribute.order_detail_attribute = @order_type.order_detail_attributes[index]
       end
-      @current_detail.subsection = Subsection.find(params[:order][:subsection_id])
+      @current_subsection = Subsection.find(params[:order][:subsection_id])
+      @current_detail.subsection = @current_subsection
+      # Acumular los montos para cada subsección
+      @amounts[@current_subsection.name] -= value[:attribute_names][-1].to_f
+    end
+    # Verificar que los fondos no sean negativos
+    @amounts.each do |key, value|
+      if value  < 0
+        # Redireccionar y mostrar error
+      end
+    end
+    # Guardar todas las entidades
+    # Guardar pedido
+    @order.save!
+    # Guardar atributos del pedido
+    @order.order_type_attribute_values.each do |order_attribute|
+      order_attribute.save!
+    end
+    # Guardar detalles
+    @order.order_details.each do |detail|
+      detail.save!
+      # Guardar atributos del detalle
+      detail.order_detail_attribute_values.each do |detail_attribute|
+        detail_attribute.save!
+      end
+    end
+    @amouts_values = []
+    params[:order][:order_details_attributes].each do |key, value|
+      @amouts_values << value[:attribute_names][-1].to_f
+    end
+    (0...@order.order_details.length).each do |index|
+      @order.order_details[index].value_histories.create(date: Time.now,
+                                                         amount: @amouts_values[index],
+                                                         value_status: ValueStatus.where(value_status_name: "Estimado"))
     end
   end
   def continue
