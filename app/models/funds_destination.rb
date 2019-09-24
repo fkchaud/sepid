63094635a9530f42495b33151a5744dcb5b84e5c
Facundo Chaud <faku_imper@hotmail.com.ar>
2019-09-23 22:57:21 -0300

    Destino de Fondos no puede tener montos negativos

--- app/models/funds_destination.rb
+++ app/models/funds_destination.rb
@@ -7,9 +7,20 @@ class FundsDestination < ApplicationRecord
   # validations
   validates :name, presence: true
   validates :project_funds_details, presence: true
-  validates_associated :project_funds_details
+  validate :project_funds_details_positive
 
   has_many :project_funds_details, inverse_of: :funds_destination
   accepts_nested_attributes_for :project_funds_details, reject_if: :all_blank
   belongs_to :funds_resolution
+
+  def project_funds_details_positive
+    return if project_funds_details.none? { |pfd| pfd.funds_amount <= 0 }
+
+    project_funds_details.each do |pfd|
+      pfd.errors.add :funds_amount, 'must be positive' if pfd.funds_amount <= 0
+    end
+    errors.add :project_funds_details, 'must be positive'
+  end
+
+  validates_associated :project_funds_details
 end
