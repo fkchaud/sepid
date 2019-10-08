86fdc1efd91f5232e3e88b99cf80e80217d6bcce
Luis <lucho7722@gmail.com>
2019-10-08 14:54:29 -0300

    Hora del sistema arreglada, segunda parte

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module SePID
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = "America/Argentina/Buenos_Aires"
-    config.active_record.default_timezone = :local
+    #config.active_record.default_timezone = :local
   end
 end
