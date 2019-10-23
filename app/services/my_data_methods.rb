7572252d531ed449211d6dce2fc31ef2c5c8168f
Luis <lucho7722@gmail.com>
2019-10-23 05:04:30 -0300

    Generar reporte, unas pequeñas mejoras, aún falta interacción

--- app/services/my_data_methods.rb
+++ app/services/my_data_methods.rb
@@ -3,6 +3,6 @@ class MyDataMethods
     Order.joins(:order_type).group('orders.order_date', 'order_types.name_type_order').order('orders.order_date').count
   end
   def self.change_status_count(properties)
-    OrderStatusHistory.joins(:order_status).group('order_status_histories.date_change_status_order', 'order_statuses.order_status_name').order('order_statuses.order_status_name').count
+    OrderStatusHistory.joins(:order_status).group('order_status_histories.date_change_status_order', 'order_statuses.order_status_name').order('order_status_histories.date_change_status_order').count
   end
 end
