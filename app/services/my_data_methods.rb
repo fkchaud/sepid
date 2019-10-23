a463dbaf08892aef639f759ad6d42e6a0c405e54
Luis <lucho7722@gmail.com>
2019-10-23 00:17:14 -0300

    Mejora interfaz gráfica y algo de reportes

--- app/services/my_data_methods.rb
+++ app/services/my_data_methods.rb
@@ -2,7 +2,7 @@ class MyDataMethods
   def self.orders_by_type(properties)
     Order.joins(:order_type).group('order_types.name_type_order').order('order_types.name_type_order').count
   end
-  def self.orders_by_status(properties)
-    Order.joins(order_status_histories: [:order_status]).group('order_statuses.order_status_name').order('order_statuses.order_status_name').count
+  def self.change_status_count(properties)
+    OrderStatusHistory.joins(:order_status).group('order_status_histories.date_change_status_order', 'order_statuses.order_status_name').order('order_statuses.order_status_name').count
   end
 end
