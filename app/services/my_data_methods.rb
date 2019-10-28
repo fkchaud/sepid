c6483530b675a33ea140b1bbab41f837cbaefabc
Luis <lucho7722@gmail.com>
2019-10-28 02:25:33 -0300

    Reportes, filtrado por rango de fechas, falta arreglar problema de selección

--- app/services/my_data_methods.rb
+++ app/services/my_data_methods.rb
@@ -1,6 +1,9 @@
 class MyDataMethods
   def self.orders_by_type(properties)
-    Order.joins(:order_type).group('orders.order_date', 'order_types.name_type_order').order('orders.order_date').count
+    ui_filters = properties[:ui_filters]
+    orders = Order.joins(:order_type).group('orders.order_date', 'order_types.name_type_order').order('orders.order_date')
+    orders = orders.where(order_date: ReportsKit.parse_date_range(ui_filters[:order_date])) if ui_filters[:order_date].present?
+    orders.count
   end
   def self.change_status_count(properties)
     OrderStatusHistory.joins(:order_status).group('order_status_histories.date_change_status_order', 'order_statuses.order_status_name').order('order_status_histories.date_change_status_order').count
