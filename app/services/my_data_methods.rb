d9f3d3007b90c178b845173b1d1f6bd00ea1ee79
Facundo Chaud <faku_imper@hotmail.com.ar>
2019-10-29 19:05:27 -0300

    Fix Date Range Picker

--- app/services/my_data_methods.rb
+++ app/services/my_data_methods.rb
@@ -5,6 +5,7 @@ class MyDataMethods
     orders = orders.where(order_date: ReportsKit.parse_date_range(ui_filters[:order_date])) if ui_filters[:order_date].present?
     orders.count
   end
+
   def self.change_status_count(properties)
     OrderStatusHistory.joins(:order_status).group('order_status_histories.date_change_status_order', 'order_statuses.order_status_name').order('order_status_histories.date_change_status_order').count
   end
