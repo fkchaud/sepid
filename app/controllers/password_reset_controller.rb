7b80baf00cf1d141172dd3af7e9dc79056b260d4
GoldenImper <faku_imper@hotmail.com.ar>
2019-11-09 21:59:03 -0300

    Pulish password_reset_controller

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -8,14 +8,14 @@ class PasswordResetController < ApplicationController
       time_difference_minutes = (Time.now - @user.password_recovery_expiration) / 60
       if time_difference_minutes < @@EXPIRATION_TIME_MINUTES
         @status = 1
-        flash[:info] = 'Puedes cambiar tu contraseña'
+        flash.now[:info] = 'Puedes cambiar tu contraseña'
       else
         @status = 2
-        flash[:danger] = 'Ha expirado el tiempo para cambiar tu contraseña'
+        flash.now[:danger] = 'Ha expirado el tiempo para cambiar tu contraseña'
       end
     else
       @status = 3
-      flash[:danger] = 'Solicitud invalida'
+      flash.now[:danger] = 'Solicitud invalida'
     end
   end
 
@@ -30,33 +30,24 @@ class PasswordResetController < ApplicationController
       @user.password_recovery_token = nil
       @user.password_recovery_expiration = nil
       if params[:user][:password] != params[:user][:password_confirmation]
-        flash[:error] = 'Contraseña nueva no coincide con la confirmada'
+        flash.now[:warning] = 'Contraseña nueva no coincide con la confirmada'
         render 'password_reset/index'
-        return
-      end
-      if params[:user][:password].length < 8 || params[:user][:password_confirmation].length < 8
-        flash[:error] = 'La contraseña debe ser mínimo de 8 caracteres'
+      elsif params[:user][:password].length < 8 || params[:user][:password_confirmation].length < 8
+        flash.now[:warning] = 'La contraseña debe ser mínimo de 8 caracteres'
         render 'password_reset/index'
-        return
-      end
-      if params[:user][:password].length > 72 || params[:user][:password_confirmation].length > 72
-        flash[:error] = 'La contraseña debe ser máximo de 72 caracteres'
+      elsif params[:user][:password].length > 72 || params[:user][:password_confirmation].length > 72
+        flash.now[:error] = 'La contraseña no debe superar los 72 caracteres'
         render 'password_reset/index'
-        return
-      end
-      if @user.save!
-        flash[:success] = 'Contraseña modificada correctamente.'
+      elsif @user.save
+        flash.now[:success] = 'Contraseña modificada correctamente.'
         redirect_to welcome_index_path
-        return
       else
-        flash[:danger] = 'Ocurrió un problema al cambiar la contraseña intenta de nuevo.'
+        flash.now[:danger] = 'Ocurrió un problema al cambiar la contraseña intenta de nuevo.'
         render 'password_reset/index'
-        return
       end
     else
-      flash[:danger] = 'Ha expirado el tiempo para cambiar tu contraseña'
+      flash.now[:danger] = 'Ha expirado el tiempo para cambiar tu contraseña'
       redirect_to welcome_index_path
-      return
     end
   end
 end
