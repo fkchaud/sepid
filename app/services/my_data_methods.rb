1b7092c3100efd483a3aedd3a256aee4e3f83eb3
Luis <lucho7722@gmail.com>
2019-11-26 20:11:04 -0300

    Reportes de pedidos por proyecto

--- app/services/my_data_methods.rb
+++ app/services/my_data_methods.rb
@@ -12,4 +12,11 @@ class MyDataMethods
     statuses = statuses.where(date_change_status_order: ReportsKit.parse_date_range(ui_filters[:date_change_status_order])) if ui_filters[:date_change_status_order].present?
     statuses.count
   end
+
+  def self.orders_by_projects(properties)
+    ui_filters = properties[:ui_filters]
+    orders = Order.joins(:project).group('orders.order_date', 'projects.project_code').order('orders.order_date')
+    orders = orders.where(order_date: ReportsKit.parse_date_range(ui_filters[:order_date])) if ui_filters[:order_date].present?
+    orders.count
+  end
 end
